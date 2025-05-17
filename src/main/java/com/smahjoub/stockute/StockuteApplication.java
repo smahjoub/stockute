c761c68f522e8efe49195586908d2af42553e116
smahjoub <saifeddine.mahjoub@outlook.com>
2025-05-17 10:54:52 +0200

    intial commit

---
+++ src/main/java/com/smahjoub/stockute/StockuteApplication.java
@@ -0,0 +1,13 @@
+package com.smahjoub.stockute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockuteApplication.class, args);
+	}
+
+}
