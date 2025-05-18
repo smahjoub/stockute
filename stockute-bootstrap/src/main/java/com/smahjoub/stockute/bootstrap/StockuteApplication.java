c2b1438ccbf2930e8fc188ff676fb3eb8985d477
smahjoub <saifeddine.mahjoub@outlook.com>
2025-05-18 10:19:41 +0200

    fix components scan and db connection string

--- stockute-bootstrap/src/main/java/com/smahjoub/stockute/bootstrap/StockuteApplication.java
+++ stockute-bootstrap/src/main/java/com/smahjoub/stockute/bootstrap/StockuteApplication.java
@@ -2,9 +2,11 @@ package com.smahjoub.stockute.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @EnableR2dbcRepositories(basePackages = "com.smahjoub.stockute.adapters.persistence")
+@ComponentScan(basePackages = "com.smahjoub.stockute")
 @SpringBootApplication
 public class StockuteApplication {
 
